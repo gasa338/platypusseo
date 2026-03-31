2b3499022ef9def650ef38bb0467a8b745f7b972
gasa338 <gasa338@gmail.com>
2026-03-31 10:20:21 +0200

    bug fix

--- wp-content/themes/maxwell-team/blocks/comparison-table/comparison-table.php
+++ wp-content/themes/maxwell-team/blocks/comparison-table/comparison-table.php
@@ -156,68 +156,68 @@ $color_mode = $data['background'] ?? 'dark';
 
         <div class="mx-auto">
             <!-- Wrapper div za skrolovanje -->
-  <div class="overflow-x-auto md:overflow-x-visible">
-            <table class="w-full border-separate border-spacing-x-2">
-                <thead class="sticky top-16 z-10">
-                    <tr>
-                        <?php foreach ($data['items'][0]['row'] as $kh => $header): ?>
-                            <?php if ($kh == 0): ?>
-                                <th class="w-48 bg-transparent"></th>
-                            <?php else: ?>
-                                <th class="text-xl font-bold text-center py-5 px-6 tracking-wide rounded-tl-2xl border-b  <?php echo $kh == 4 ? 'bg-hero text-white border-border' : 'bg-slate-50 text-primary border-border'; ?>">
-                                    <?php echo $header['name']; ?>
-                                </th>
+            <div class="overflow-x-auto md:overflow-x-visible">
+                <table class="w-full border-separate border-spacing-x-2">
+                    <thead class="sticky top-16 z-10">
+                        <tr>
+                            <?php foreach ($data['items'][0]['row'] as $kh => $header): ?>
+                                <?php if ($kh == 0): ?>
+                                    <th class="w-48 bg-transparent"></th>
+                                <?php else: ?>
+                                    <th class="text-xl font-bold text-center py-5 px-6 tracking-wide rounded-tl-2xl border-b  <?php echo $kh == 4 ? 'bg-hero text-white border-border' : 'bg-slate-50 text-primary border-border'; ?>">
+                                        <?php echo $header['name']; ?>
+                                    </th>
+                                <?php endif; ?>
+                            <?php endforeach; ?>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <?php foreach ($data['items'] as $key => $item): ?>
+                            <?php if ($key >= 1): ?>
+                                <tr>
+                                    <?php foreach ($item['row'] as $k => $el): ?>
+                                        <?php if ($k == 0): ?>
+                                            <td class="py-6 pr-6 pl-4 align-middle">
+                                                <span class="text-primary text-xl font-semibold"><?php echo $el['name']; ?></span>
+                                            </td>
+                                        <?php else: ?>
+                                            <?php
+                                            // Odredi klase na osnovu indeksa
+                                            $tdClasses = "text-center py-6 px-4 align-middle  ";
+                                            if ($k == 4) {
+                                                $tdClasses .= "bg-hero";
+                                            } else {
+                                                $tdClasses .= "bg-slate-50";
+                                            }
+                                            ?>
+                                            <td class="<?php echo $tdClasses; ?>">
+                                                <div class="flex items-center justify-center gap-2">
+                                                    <span class="text-lg <?php echo $k == 4 ? 'text-white' : 'text-primary'; ?>">
+                                                        <?php echo $el['name']; ?>
+                                                    </span>
+                                                    <?php if (!empty($el['popup'])): ?>
+                                                        <div class="tooltip-wrap">
+                                                            <div class="bg-accent text-white rounded-full inline-flex items-center justify-center">
+                                                                <svg class="w-4 h-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                                                    <path d="M12 8V12" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
+                                                                    <circle cx="12" cy="16" r="1" fill="currentColor" />
+                                                                </svg>
+                                                            </div>
+                                                            <div class="tip"><?php echo $el['popup']; ?></div>
+                                                        </div>
+                                                    <?php endif; ?>
+                                                </div>
+                                            </td>
+                                        <?php endif; ?>
+                                    <?php endforeach; ?>
+                                </tr>
                             <?php endif; ?>
                         <?php endforeach; ?>
-                    </tr>
-                </thead>
 
-                <tbody>
-                    <?php foreach ($data['items'] as $key => $item): ?>
-                        <?php if ($key >= 1): ?>
-                            <tr>
-                                <?php foreach ($item['row'] as $k => $el): ?>
-                                    <?php if ($k == 0): ?>
-                                        <td class="py-6 pr-6 pl-4 align-middle">
-                                            <span class="text-primary text-xl font-semibold"><?php echo $el['name']; ?></span>
-                                        </td>
-                                    <?php else: ?>
-                                        <?php
-                                        // Odredi klase na osnovu indeksa
-                                        $tdClasses = "text-center py-6 px-4 align-middle  ";
-                                        if ($k == 4) {
-                                            $tdClasses .= "bg-hero";
-                                        } else {
-                                            $tdClasses .= "bg-slate-50";
-                                        }
-                                        ?>
-                                        <td class="<?php echo $tdClasses; ?>">
-                                            <div class="flex items-center justify-center gap-2">
-                                                <span class="text-lg <?php echo $k == 4 ? 'text-white' : 'text-primary'; ?>">
-                                                    <?php echo $el['name']; ?>
-                                                </span>
-                                                <?php if (!empty($el['popup'])): ?>
-                                                    <div class="tooltip-wrap">
-                                                        <div class="bg-accent text-white rounded-full inline-flex items-center justify-center">
-                                                            <svg class="w-4 h-4" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                                <path d="M12 8V12" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
-                                                                <circle cx="12" cy="16" r="1" fill="currentColor" />
-                                                            </svg>
-                                                        </div>
-                                                        <div class="tip"><?php echo $el['popup']; ?></div>
-                                                    </div>
-                                                <?php endif; ?>
-                                            </div>
-                                        </td>
-                                    <?php endif; ?>
-                                <?php endforeach; ?>
-                            </tr>
-                        <?php endif; ?>
-                    <?php endforeach; ?>
-
-                </tbody>
-            </table>
-        </div>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </section>
