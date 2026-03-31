06517cd77f0ee2923a2fb72e15a1fd374dbafbfb
gasa338 <gasa338@gmail.com>
2026-03-31 11:53:50 +0200

    ispravka za feature 4

--- wp-content/themes/maxwell-team/blocks/features-4/features-4.php
+++ wp-content/themes/maxwell-team/blocks/features-4/features-4.php
@@ -35,10 +35,10 @@ $color_mode = $data['color_mode'] ?? 'dark';
                 <?php endif; ?>
                 <?php echo _heading($data['title'], 'mb-6 ' . esc_attr($color_mode == 'dark' ? 'text-white' : '')) ?>
                 <?php if (!empty($data['text'])): ?>
-                    <div class="<?php echo esc_attr($color_mode == 'dark' ? 'text-white/60 [&_li]:!text-white [&_span]:!text-white [&_strong]:!text-white' : 'text-muted-foreground [&_li]:!text-foreground [&_span]:!text-muted-foreground [&_strong]:!text-muted-foreground'); ?> text-lg mb-10 leading-relaxed maxwell-content"><?php echo apply_filters('the_content', $data['text']); ?></div>
+                    <div class="<?php echo esc_attr($color_mode == 'dark' ? 'text-white/60 [&_li]:!text-white [&_span]:!text-white [&_strong]:!text-white' : 'text-muted-foreground [&_li]:!text-foreground [&_span]:!text-muted-foreground [&_strong]:!text-muted-foreground'); ?> text-lg leading-relaxed maxwell-content"><?php echo apply_filters('the_content', $data['text']); ?></div>
                 <?php endif; ?>
                 <?php if (!empty($data['features'])): ?>
-                    <div class="grid sm:grid-cols-2 gap-6 mb-6">
+                    <div class="grid sm:grid-cols-2 gap-6 my-6">
                         <?php foreach ($data['features'] as $key => $value): ?>
                             <div class="flex gap-4">
                                 <div class="w-12 h-12 rounded-xl flex items-center justify-center shrink-0 bg-accent">
